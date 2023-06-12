b84cb4938b13ff460539434704b3ee8f76722519
NMason1990 <nathanielmason90@gmail.com>
2023-06-12 14:31:59 -0400

    api request in dataframe, stuck here

--- Data_C2_Kc_1.py
+++ Data_C2_Kc_1.py
@@ -7,37 +7,39 @@ token = '0e556d07bc8f4aa14ce99df24bacbf6f58201b13257e797d7f8497d42ecc0e79'
 
 secret_token = '696a6f47212d93f88cef71238cfb3d1cd357905b3e34eabd2bbc49124553c129'
 
+# Generate the Unix timestamp
 timestamp = int(time.time())
 
+# Construct the query string
 query_string = f'time={timestamp}'
 
+# Generate the hash
 hash_string = secret_token + str(timestamp)
 hash_value = hashlib.sha512(hash_string.encode()).hexdigest()
 
+# Construct the URL with the query string and hash
 url = f'https://api.fabdb.net/cards?{query_string}&hash={hash_value}'
 
+# Set the request headers
 headers = {
     'Authorization': f'Bearer {token}',
     'Accept': 'application/json'
 }
 
+# Send the GET request
 response = requests.get(url, headers=headers)
 
 # Check the response status code
 print(f'Response status code: {response.status_code}')
 
-# Check the response content
-print(f'Response content: {response.content}')
+# Convert response to JSON
+data = response.json()
 
-if response.status_code == 200:
-    # Request was successful
-    data = response.json()
-    print(f'Response data: {data}')
-    if isinstance(data, list):
-        # Convert non-dictionary items to dictionaries
-        data = [item if isinstance(item, dict) else {'data': item} for item in data]
-        df = pd.DataFrame(data)
-        print(df)
-else:
-    # Request failed
-    print(f'Request failed with status code: {response.status_code}')
+# Extract the 'data' field from the response dictionary
+data_records = data['data']
+
+# Create a DataFrame from the extracted data records
+df = pd.DataFrame(data_records)
+
+# Display the DataFrame
+print(df)
