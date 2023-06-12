c73318744626341b06bf4a57ec06126ee08a5643
NMason1990 <nathanielmason90@gmail.com>
2023-06-12 13:15:53 -0400

    updated api request

--- Data_C2_Kc_1.py
+++ Data_C2_Kc_1.py
@@ -1,20 +1,43 @@
-
-# importing packages and requests from api
-import pandas as pd
-from pandas import json_normalize
 import requests
+import hashlib
+import time
+import pandas as pd
 
-url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/types/Minion"
+token = '0e556d07bc8f4aa14ce99df24bacbf6f58201b13257e797d7f8497d42ecc0e79'
+
+secret_token = '696a6f47212d93f88cef71238cfb3d1cd357905b3e34eabd2bbc49124553c129'
+
+timestamp = int(time.time())
+
+query_string = f'time={timestamp}'
+
+hash_string = secret_token + str(timestamp)
+hash_value = hashlib.sha512(hash_string.encode()).hexdigest()
+
+url = f'https://api.fabdb.net/cards?{query_string}&hash={hash_value}'
 
 headers = {
-	"X-RapidAPI-Key": "dfafdd2951msh214c441e47ba622p120bf6jsn0127ae649412",
-	"X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+    'Authorization': f'Bearer {token}',
+    'Accept': 'application/json'
 }
 
 response = requests.get(url, headers=headers)
 
-print(response.json())
+# Check the response status code
+print(f'Response status code: {response.status_code}')
 
-df = pd.DataFrame(response)
+# Check the response content
+print(f'Response content: {response.content}')
 
-df[df['Rarity'] == 'Legendary']
+if response.status_code == 200:
+    # Request was successful
+    data = response.json()
+    print(f'Response data: {data}')
+    if isinstance(data, list):
+        # Convert non-dictionary items to dictionaries
+        data = [item if isinstance(item, dict) else {'data': item} for item in data]
+        df = pd.DataFrame(data)
+        print(df)
+else:
+    # Request failed
+    print(f'Request failed with status code: {response.status_code}')
